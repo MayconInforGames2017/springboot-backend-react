de810cae98f52946d2572f18e4823a839b10e9cd
Maycon R Santana <maycon_gus2009@hotmail.com>
2020-07-26 13:08:32 -0300

    Metodo salvar Funcionario

--- src/main/java/com/mayconinforgames/spring/controller/FuncionarioController.java
+++ src/main/java/com/mayconinforgames/spring/controller/FuncionarioController.java
@@ -3,13 +3,17 @@ package com.mayconinforgames.spring.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mayconinforgames.spring.model.Funcionario;
 import com.mayconinforgames.spring.repository.FuncionarioRepository;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class FuncionarioController {
@@ -20,6 +24,11 @@ public class FuncionarioController {
 	@GetMapping("/funcionarios")
 	public List<Funcionario> getAllFuncionarios() {
 		return funcionarioRepository.findAll();
+	}
+	
+	@PostMapping("/funcionarios")
+	public Funcionario inserirFuncionario(@RequestBody Funcionario funcionario) {
+		return funcionarioRepository.save(funcionario);
 	}
 
 }
