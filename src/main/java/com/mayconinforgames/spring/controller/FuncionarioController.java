5d3d563f19248b84e89e7a13b539f6369c1b2863
Maycon R Santana <maycon_gus2009@hotmail.com>
2020-07-26 21:19:26 -0300

    Conexao da API REST com o React

--- src/main/java/com/mayconinforgames/spring/controller/FuncionarioController.java
+++ src/main/java/com/mayconinforgames/spring/controller/FuncionarioController.java
@@ -3,7 +3,7 @@ package com.mayconinforgames.spring.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.mayconinforgames.spring.model.Funcionario;
 import com.mayconinforgames.spring.repository.FuncionarioRepository;
 
-//@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class FuncionarioController {
