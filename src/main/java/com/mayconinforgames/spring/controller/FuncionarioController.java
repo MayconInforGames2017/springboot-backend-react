65b45d091e80ff1c055a3d1fdb495ebe532e25a3
Maycon R Santana <maycon_gus2009@hotmail.com>
2020-07-29 21:11:24 -0300

    Implementando lista Funcionario pelo id

--- src/main/java/com/mayconinforgames/spring/controller/FuncionarioController.java
+++ src/main/java/com/mayconinforgames/spring/controller/FuncionarioController.java
@@ -3,13 +3,16 @@ package com.mayconinforgames.spring.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mayconinforgames.spring.exception.ResourceNotFoundException;
 import com.mayconinforgames.spring.model.Funcionario;
 import com.mayconinforgames.spring.repository.FuncionarioRepository;
 
@@ -25,10 +28,18 @@ public class FuncionarioController {
 	public List<Funcionario> getAllFuncionarios() {
 		return funcionarioRepository.findAll();
 	}
-	
+
 	@PostMapping("/funcionarios")
 	public Funcionario createFuncionario(@RequestBody Funcionario funcionario) {
 		return funcionarioRepository.save(funcionario);
+	}
+
+	@GetMapping("/funcionarios/{id}")
+	public ResponseEntity<Funcionario> getFuncionarioById(@PathVariable Long id) {
+
+		Funcionario funcionario = funcionarioRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Código do funcionario não encontrado: " + id));
+		return ResponseEntity.ok(funcionario);
 	}
 
 }
